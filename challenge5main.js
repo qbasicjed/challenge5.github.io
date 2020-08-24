17b243db5f0882e1c186f52b5322e71fabdf41a7
qbasicjed <40079096+qbasicjed@users.noreply.github.com>
2020-08-24 17:04:00 -0500

    Add files via upload

--- challenge5main.js
+++ challenge5main.js
@@ -44,7 +44,6 @@ require([
                 window.location.reload();
             });
 
-
             //CheckSignInStatus returns Promise<Credential> or error callback, 
             //depending on if the user has signed in or not.
             //If it returns a credential, the function is run to cease displaying "sign=in" text
@@ -53,12 +52,27 @@ require([
                 //login/logout 
                 dom.byId('anonymousPanel').style.display = 'none';
                 dom.byId('personalizedPanel').style.display = 'block';
-
+                dom.byId('userId').innerText = Credential.username;
                 //change the heading from "no items found"  
                 dom.byId('itemsList').element.textContent = 'Here are your items';
                 //content
                 dom.byId('itemsList').style.display = 'flex';
 
+                var portal = new Portal();
+// Setting authMode to immediate signs the user in once loaded
+portal.authMode = "immediate";
+// Once loaded, user is signed in
+portal.load().then(function() {
+  // Create query parameters for the portal search
+  var queryParams = new PortalQueryParams({
+    query: "owner:" + portal.user.username,
+    sortField: "type",
+    num: 100
+  });
+
+  // Query the items based on the queryParams created from portal above
+  portal.queryItems(queryParams).then(createGallery);
+});
             });
         }
             
