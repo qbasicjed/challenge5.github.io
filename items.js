7f68cfa9bf502f1a470f0c2554ba54927f6313fd
qbasicjed <40079096+qbasicjed@users.noreply.github.com>
2020-09-07 22:36:52 -0500

    Add files via upload

--- items.js
+++ items.js
@@ -90,7 +90,7 @@ require([
         //append to the htmlFragment with html code to produce for each item.
         htmlFragment +=
           //start tag for the produced container for an item
-          '<div class="esri-item-container">' +
+          '<div class="esri-item-container" onclick="addTag(item)">' +
           //if the item.thumbnailUrl is present and valid
           (item.thumbnailUrl
             //add a div with the image at the url as the background image
@@ -114,7 +114,7 @@ require([
             //append a new tag to the string  
             tagSection +=
               (tag
-                ? '<div class="tags">' + (tag || "") + ", </div>"
+                ? '<div class="tags action">' + (tag || "") + ", </div>"
                 //in case there are no tags
                 : '<div class="tags">No Tags</div>'
               );
@@ -126,6 +126,14 @@ require([
           htmlFragment += tagSection +
           "</div>";
 
+          function addTag(item){
+            document.getElementById("test").innerHTML = 
+            item.title
+            //show it below the image
+            ? '<div class="esri-title">' + (item.title || "") + "</div>"
+            //otherwise show alternate text
+            : '<div class="esri-title esri-null-title">Title not available</div>');
+          }
       });
       //generate the code string specified in htmlFragment
       document.getElementById("itemGallery").innerHTML = htmlFragment;
